50e3d26e68d20d865ba480acc67e0ed34278bf7f
Stas Dubinin <dubinin.stas@gmail.com>
2023-03-21 22:14:40 +0400

    pipec

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,31 +1,72 @@
-import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
+import {
+  Tab,
+  ConstructorElement,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 import React from "react";
 import styles from "./burger-ingredients.module.css";
+import { data } from "../utils/utils.js";
 
-const BurgerIngredients = () => {
-  const [current, setCurrent] = React.useState("one");
-  return (
-    <section className={styles["section-burger-menu"]}>
-      <h2>Соберите бургер</h2>
-      <div style={{ display: "flex" }}>
-        <Tab value="one" active={current === "one"} onClick={setCurrent}>
-          Булки
-        </Tab>
-        <Tab value="two" active={current === "two"} onClick={setCurrent}>
-          Соусы
-        </Tab>
-        <Tab value="three" active={current === "three"} onClick={setCurrent}>
-          Начинки
-        </Tab>
-      </div>
-      <div className={styles["section-burger-menu__ingridients"]}>
-        <h3 className="text text_type_main-large">Булки</h3>
-        <div className={styles["section-burger-menu__ingridients"]}>
+class BurgerIngredients extends React.Component {
+  state = {
+    current: "one",
+    setCurrent: "",
+    dataIngr: data,
+  };
 
+  creatList = (arrayIngr) => {
+    return arrayIngr.reduce((items, item) => {
+      items += 
+        '<li>'+
+          <ConstructorElement
+            type={"type"}
+            isLocked={true}
+            text="Краторная булка N-200i (верх)"
+            price={200}
+            thumbnail={item.image}
+          />
+        +'</li>'
+      );
+
+      return items;
+    });
+  };
+
+  render() {
+    return (
+      <section className={styles["section-burger-menu"]}>
+        <h2>Соберите бургер</h2>
+        <div style={{ display: "flex" }}>
+          <Tab
+            value="one"
+            active={this.state.current === "one"}
+            onClick={this.state.setCurrent}
+          >
+            Булки
+          </Tab>
+          <Tab
+            value="two"
+            active={this.state.current === "two"}
+            onClick={this.state.setCurrent}
+          >
+            Соусы
+          </Tab>
+          <Tab
+            value="three"
+            active={this.state.current === "three"}
+            onClick={this.state.setCurrent}
+          >
+            Начинки
+          </Tab>
         </div>
-      </div>
-    </section>
-  );
-};
+        <div className={styles["section-burger-menu__ingridients"]}>
+          <h3 className="text text_type_main-large">Булки</h3>
+          <ul className={styles["section-burger-menu__ingridients-list"]}>
+            {this.creatList(this.state.dataIngr)}
+          </ul>
+        </div>
+      </section>
+    );
+  }
+}
 
 export default BurgerIngredients;
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import styles from "./index.css";
-import App from "./App";
+import App from "./App.jsx";
 import reportWebVitals from "./reportWebVitals";
-import AppHeader from "./components/app-header/app-header.jsx";
-import BurgerIngridients from "./components/burger-ingredients/burger-ingredients.jsx"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <body className={styles.body}>
-    <AppHeader />
-    <main>
-      <BurgerIngridients/>
-    </main>
+    <App />
   </body>
 );
 
