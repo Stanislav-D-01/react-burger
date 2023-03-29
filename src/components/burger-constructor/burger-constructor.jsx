5bf85533dcfdd7bb7626d9b755a24a589136e496
Stas Dubinin <dubinin.stas@gmail.com>
2023-03-29 18:29:22 +0400

    func components

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -6,28 +6,23 @@ import styles from "./burger-constructor.module.css";
 import priceSym from "../../image/Subtract_constructor.svg";
 import React from "react";
 import PropTypes from "prop-types";
+import { dataPropTypes } from "../utils/utils";
 
-const dataPropTypes = PropTypes.shape({
-  _id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  proteins: PropTypes.number,
-  fat: PropTypes.number,
-  carbohydrates: PropTypes.number,
-  calories: PropTypes.number,
-  price: PropTypes.number,
-  image: PropTypes.string,
-  image_mobile: PropTypes.string,
-  image_large: PropTypes.string,
-  __v: PropTypes.number,
-});
 
-class BurgerConstructor extends React.Component {
-  state = {
-    total: 0,
-  };
 
-  renderListOrder(data) {
+function BurgerConstructor(props) {
+  const [ingr, setIngr] = React.useState([]);
+  const [total, setTotal] = React.useState(0);
+
+
+React.useEffect(()=>{
+  setIngr(...ingr,props.data)
+   totalPrice(props.data)
+},[props.data])
+
+
+
+ const renderListOrder = (data) => {
     return data.map((element) => {
       if (element.type !== "bun") {
         return (
@@ -42,59 +37,62 @@ class BurgerConstructor extends React.Component {
       }
     });
   }
-  totalPrice(data) {
+  
+  const totalPrice=(data)=> {
     const sum = data.reduce((sum, element) => sum + element.price, 0);
-    this.setState({ total: sum });
+   setTotal(sum)
   }
-  componentDidMount() {
-    this.totalPrice(this.props.data);
-  }
+ 
 
-  render() {
-    return (
+  console.log(ingr)
+  
+  if (ingr.length>0) {
+  return (
+      
       <section className={styles["burger-constructor"]}>
         <ul className={styles["burger-constructor__list"]}>
           <li
-            key={this.props.data[0]._id}
+            key={ingr[0]._id}
             className={`${styles["burger-constructor__point"]} ${styles["burger-constructor__point_type_lock"]}`}
           >
             <ConstructorElement
               type="top"
               isLocked={true}
-              text={this.props.data[0].name}
-              price={this.props.data[0].price}
-              thumbnail={this.props.data[0].image}
+              text={ingr[0].name}
+              price={ingr[0].price}
+              thumbnail={ingr[0].image}
             />
           </li>
-          {this.renderListOrder(this.props.data)}
+          {renderListOrder(ingr)}
           <li
-            key={this.props.data[this.props.data.length - 1]._id}
+            key={ingr[1]._id}
             className={`${styles["burger-constructor__point"]} ${styles["burger-constructor__point_type_lock"]}`}
           >
             <ConstructorElement
               type="bottom"
               isLocked={true}
-              text={this.props.data[this.props.data.length - 1].name}
-              price={this.props.data[this.props.data.length - 1].price}
-              thumbnail={this.props.data[this.props.data.length - 1].image}
+              text={ingr[1].name}
+              price={ingr[1].price}
+              thumbnail={ingr[1].image}
             />
           </li>
         </ul>
         <div className={styles["burger-constructor__total-price-block"]}>
           <p className="text text_type_digits-medium mr-10">
-            {this.state.total} <img className="pl-2" src={priceSym} />
+            {total} <img className="pl-2" src={priceSym} />
           </p>
           <Button htmlType="button" type="primary" size="medium">
             Оформить заказ
           </Button>
         </div>
-      </section>
-    );
+  </section>)}
+   
+  
   }
-}
 
 export default BurgerConstructor;
 
+
 BurgerConstructor.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.shape(dataPropTypes)),
+  ingr: PropTypes.arrayOf(PropTypes.shape(dataPropTypes)),
 };
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -7,28 +7,19 @@ import styles from "./burger-ingredients.module.css";
 import priceSym from "../../image/Subtract.svg";
 import React from "react";
 import PropTypes from "prop-types";
+import { dataPropTypes, urlAdress } from "../utils/utils.js";
 
-const dataPropTypes = PropTypes.shape({
-  _id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
-  proteins: PropTypes.number,
-  fat: PropTypes.number,
-  carbohydrates: PropTypes.number,
-  calories: PropTypes.number,
-  price: PropTypes.number,
-  image: PropTypes.string,
-  image_mobile: PropTypes.string,
-  image_large: PropTypes.string,
-  __v: PropTypes.number,
-});
+function BurgerIngredients(props) {
+  const [current, setCurrent] = React.useState("bun");
+ 
 
-class BurgerIngredients extends React.Component {
-  state = {
-    current: "one",
-  };
 
-  loadingridients = (data, type) => {
+
+ 
+
+  
+
+  const loadingridients = (data, type) => {
     return data.map((element) => {
       if (element.type === type) {
         return (
@@ -46,53 +37,46 @@ class BurgerIngredients extends React.Component {
     });
   };
 
-  render() {
+  const renderIngridients = (data, type) => {
     return (
-      <section className={styles["section-burger-ingridients"]}>
-        <h2 className="text text_type_main-large mt-10">Соберите бургер</h2>
-        <div style={{ display: "flex" }}>
-          <Tab
-            value="one"
-            active={this.state.current === "one"}
-            onClick={"one"}
-          >
-            Булки
-          </Tab>
-          <Tab
-            value="two"
-            active={this.state.current === "two"}
-            onClick={"one"}
-          >
-            Соусы
-          </Tab>
-          <Tab
-            value="three"
-            active={this.state.current === "three"}
-            onClick={"one"}
-          >
-            Начинки
-          </Tab>
-        </div>
-
-        <div className={styles["section-burger-ingridients__list"]}>
-          <h3 className="text text_type_main-medium mt-10">Булки</h3>
-          <ul className={styles["section-burger-menu__cards-ingridients"]}>
-            {this.loadingridients(this.props.data, "bun")}
-          </ul>
-
-          <h3 className="text text_type_main-medium mt-10">Соусы</h3>
-          <ul className={styles["section-burger-menu__cards-ingridients"]}>
-            {this.loadingridients(this.props.data, "sauce")}
-          </ul>
-
-          <h3 className="text text_type_main-medium mt-10">Начинки</h3>
-          <ul className={styles["section-burger-menu__cards-ingridients"]}>
-            {this.loadingridients(this.props.data, "main")}
-          </ul>
-        </div>
-      </section>
+      <>
+        <h3 className="text text_type_main-medium mt-10">
+          {current === "bun"
+            ? "Булки"
+            : current === "sauce"
+            ? "Соусы"
+            : "Начинки"}
+        </h3>
+        <ul className={styles["section-burger-menu__cards-ingridients"]}>
+          {loadingridients(data, type)}
+        </ul>
+      </>
     );
-  }
+  };
+
+  return (
+    <section className={styles["section-burger-ingridients"]}>
+      <h2 className="text text_type_main-large mt-10">Соберите бургер</h2>
+      <div style={{ display: "flex" }}>
+        <Tab value="bun" active={current === "bun"} onClick={setCurrent}>
+          Булки
+        </Tab>
+        <Tab value="sauce" active={current === "sauce"} onClick={setCurrent}>
+          Соусы
+        </Tab>
+        <Tab value="main" active={current === "main"} onClick={setCurrent}>
+          Начинки
+        </Tab>
+      </div>
+
+    
+        <div className={styles["section-burger-ingridients__list"]}>
+          {renderIngridients(props.data, current)}
+        </div>
+      
+  
+    </section>
+  );
 }
 
 export default BurgerIngredients;
