ed2c3431f6cf3ec0d20f0cb6b51f73328fc26d19
Stas Dubinin <dubinin.stas@gmail.com>
2023-04-12 22:10:22 +0400

    context

---
+++ src/components/burger-constructor/burger-constructor-context.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+
+export const ConstructorIngridientsContext = React.createContext();
+export const ConstructorTotalPrice = React.createContext();
--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -10,15 +10,18 @@ import { dataPropTypes } from "../utils/utils";
 import OrderDetails from "../order-details/order-details";
 import ModalOverlay from "../modal-overlay/modal-overlay";
 import Modal from "../modal/modal";
+import {
+  ConstructorIngridientsContext,
+  ConstructorTotalPrice,
+} from "./burger-constructor-context";
 
 function BurgerConstructor({ data }) {
   const [ingr, setIngr] = React.useState([]);
   const [total, setTotal] = React.useState(0);
   const [stateModal, setStateModal] = React.useState(false);
+  const [numBun, setNumBun] = React.useState(0);
 
   React.useEffect(() => {
-    setIngr(...ingr, data);
-    totalPrice(data);
     document.addEventListener("keydown", (e) => {
       if (e.key === "Escape") {
         setStateModal(false);
@@ -33,6 +36,7 @@ function BurgerConstructor({ data }) {
   const renderListOrder = (data) => {
     return data.map((element) => {
       if (element.type !== "bun") {
+        setIngr(...ingr, element);
         return (
           <li key={element._id} className={styles["burger-constructor__point"]}>
             <ConstructorElement
@@ -51,58 +55,61 @@ function BurgerConstructor({ data }) {
     setTotal(sum);
   };
 
-  if (ingr.length > 0) {
+  if (data.length > 0) {
     return (
       <>
         <section className={styles["burger-constructor"]}>
           <ul className={styles["burger-constructor__list"]}>
             <li
-              key={ingr[0]._id}
+              key={data[0]._id}
               className={`${styles["burger-constructor__point"]} ${styles["burger-constructor__point_type_lock"]}`}
             >
               <ConstructorElement
                 type="top"
                 isLocked={true}
-                text={`${ingr[0].name} (верх)`}
-                price={ingr[0].price}
-                thumbnail={ingr[0].image}
+                text={`${data[0].name} (верх)`}
+                price={data[0].price}
+                thumbnail={data[0].image}
               />
             </li>
-            {renderListOrder(ingr)}
+            {renderListOrder({ data })}
             <li
-              key={ingr[1]._id}
+              key={data[1]._id}
               className={`${styles["burger-constructor__point"]} ${styles["burger-constructor__point_type_lock"]}`}
             >
               <ConstructorElement
                 type="bottom"
                 isLocked={true}
-                text={`${ingr[0].name} (низ)`}
-                price={ingr[0].price}
-                thumbnail={ingr[0].image}
+                text={`${data[0].name} (низ)`}
+                price={data[0].price}
+                thumbnail={data[0].image}
               />
             </li>
           </ul>
-          <div className={styles["burger-constructor__total-price-block"]}>
-            <p className="text text_type_digits-medium mr-10">
-              {total} <img className="pl-2" src={priceSym} />
-            </p>
-            <Button
-              onClick={toggleModal}
-              htmlType="button"
-              type="primary"
-              size="medium"
-            >
-              Оформить заказ
-            </Button>
-          </div>
-          {stateModal && (
-            <>
-              <ModalOverlay onClose={toggleModal} />
-              <Modal closeModal={toggleModal} name={""}>
-                <OrderDetails numOrder={"000001"} />
-              </Modal>
-            </>
-          )}
+          <ConstructorIngridientsContext.Provider>
+            <div className={styles["burger-constructor__total-price-block"]}>
+              <p className="text text_type_digits-medium mr-10">
+                {total} <img className="pl-2" src={priceSym} />
+              </p>
+
+              <Button
+                onClick={toggleModal}
+                htmlType="button"
+                type="primary"
+                size="medium"
+              >
+                Оформить заказ
+              </Button>
+            </div>
+            {stateModal && (
+              <>
+                <ModalOverlay onClose={toggleModal} />
+                <Modal closeModal={toggleModal} name={""}>
+                  <OrderDetails numOrder={"000001"} />
+                </Modal>
+              </>
+            )}
+          </ConstructorIngridientsContext.Provider>
         </section>
       </>
     );
